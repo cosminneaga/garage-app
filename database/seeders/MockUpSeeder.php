a05d861bf98ad3b847b35e03fdda477df0f2a2a8
Cosmin Neaga <cosmin.neaga91@gmail.com>
2026-03-22 09:52:21 +0200

    s

--- database/seeders/MockUpSeeder.php
+++ database/seeders/MockUpSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Address;
 use App\Models\Company;
+use App\Models\Contact;
 use App\Models\Country;
 use App\Models\Product;
 use App\Models\User;
@@ -17,35 +19,49 @@ class MockUpSeeder extends Seeder
     {
         $this->call(PermissionsSeeder::class);
 
+        $country = Country::factory()->create();
+        $contact = Contact::factory(5)->create();
+        $address = Address::factory(5)->create([
+            'country_id' => $country,
+        ]);
+
+        // admin
         $adminUser = User::factory()->create([
             'name' => 'Super Admin',
             'email' => 'admin@garage.com',
             'active' => true,
         ]);
+        $adminUser->addresses()->attach([$address[0]->id, $address[1]->id]);
+        $adminUser->contacts()->attach([$contact[0]->id, $contact[1]->id]);
         $adminUser->assignRole('super');
 
+        // editor
         $editorUser = User::factory()->create([
             'name' => 'Editor Admin',
             'email' => 'editor@garage.com',
             'active' => true,
         ]);
+        $editorUser->addresses()->attach($address[2]->id);
+        $editorUser->contacts()->attach($contact[2]->id);
         $editorUser->assignRole('editor');
 
+        // viewer
         $viewerUser = User::factory()->create([
             'name' => 'Viewer Admin',
             'email' => 'viewer@garage.com',
             'active' => true,
         ]);
+        $viewerUser->addresses()->attach($address[3]->id);
+        $viewerUser->contacts()->attach($contact[3]->id);
         $viewerUser->assignRole('viewer');
 
-        // country
-        $country = Country::factory()->create();
-
         // company
-        $company = Company::factory()->create([
-            'country_id' => $country,
-        ]);
+        $company = Company::factory()->create();
+        $company->addresses()->attach($address[4]->id);
+        $company->contacts()->attach($contact[4]->id);
         $adminUser->companies()->attach($company->id);
+        $editorUser->companies()->attach($company->id);
+        $viewerUser->companies()->attach($company->id);
 
         // products
         $products = Product::factory(10)->create([
