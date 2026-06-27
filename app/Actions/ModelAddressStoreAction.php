16d1baf9e333c3e1e9802ce16afb8a74edeb5c03
Cosmin Neaga <57816741+cosminneaga@users.noreply.github.com>
2026-06-27 20:39:42 +0300

    Update ModelAddressStoreAction.php
    check added to ensure finding resource only if coordinates are provided

--- app/Actions/ModelAddressStoreAction.php
+++ app/Actions/ModelAddressStoreAction.php
@@ -16,12 +16,14 @@ class ModelAddressStoreAction
     public function handle(array $attributes, Model $model): Throwable|Address
     {
         $coordinates = Coordinates::format($attributes['coordinates']);
-        $exists = Address::query()
-            ->whereRaw('ST_Y(coordinates) = ?', [$coordinates->latitude], 'and')
-            ->whereRaw('ST_X(coordinates) = ?', [$coordinates->longitude], 'and')
-            ->exists();
-        if ($exists) {
-            throw new Exception('Address already exists under same coordinates.');
+        if ($coordinates) {
+            $exists = Address::query()
+                ->whereRaw('ST_Y(coordinates) = ?', [$coordinates->latitude], 'and')
+                ->whereRaw('ST_X(coordinates) = ?', [$coordinates->longitude], 'and')
+                ->exists();
+            if ($exists) {
+                throw new Exception('Address already exists under same coordinates.');
+            }
         }
 
         return DB::transaction(function () use ($attributes, $model, $coordinates) {
