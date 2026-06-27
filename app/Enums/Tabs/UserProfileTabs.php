229d2c80165a0fc32ddc8f7dddf433246c837fc9
Cosmin Neaga <57816741+cosminneaga@users.noreply.github.com>
2026-06-27 20:41:15 +0300

    Create UserProfileTabs.php
    added user profile tabs

---
+++ app/Enums/Tabs/UserProfileTabs.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums\Tabs;
+
+use Illuminate\Support\Collection;
+
+enum UserProfileTabs: string
+{
+    case DETAILS = 'details';
+    case STATISTICS = 'statistics';
+    case CONTACTS = 'contacts';
+    case ADDRESSES = 'addresses';
+    case SETTINGS = 'settings';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::DETAILS => 'Details',
+            self::STATISTICS => 'Statistics',
+            self::CONTACTS => 'Contacts',
+            self::ADDRESSES => 'Addresses',
+            self::SETTINGS => 'Settings',
+        };
+    }
+
+    public function slug(): string
+    {
+        return match ($this) {
+            self::DETAILS => 'details',
+            self::STATISTICS => 'statistics',
+            self::CONTACTS => 'contacts',
+            self::ADDRESSES => 'addresses',
+            self::SETTINGS => 'settings',
+        };
+    }
+
+    public static function ui(): array
+    {
+        return new Collection(self::cases())
+            ->map(fn ($case) => [
+                'value' => $case->value,
+                'label' => $case->label(),
+                'slug' => $case->slug(),
+            ])->toArray();
+    }
+
+    public static function findByValue(?string $value): false|string
+    {
+        if (self::tryFrom($value)) {
+            return self::from($value)->value;
+        }
+
+        return false;
+    }
+}
