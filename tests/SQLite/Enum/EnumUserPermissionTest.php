2123f06ce46d1590c4dcab08d9a7f1b59be650a9
Cosmin Neaga <57816741+cosminneaga@users.noreply.github.com>
2026-07-14 21:40:43 +0300

    WIP - tests updates

--- tests/SQLite/Enum/EnumUserPermissionTest.php
+++ tests/SQLite/Enum/EnumUserPermissionTest.php
@@ -6,6 +6,48 @@ use App\Models\User;
 use Illuminate\Support\Collection;
 use Spatie\Permission\Models\Permission;
 
+test('references', function () {
+    expect(UserPermission::references())->toMatchArray(
+        Collection::make(UserPermission::cases())->map(fn($case) => $case->value)
+    );
+});
+
+test('name: success', function () {
+    expect(UserPermission::name(UserPermission::USER, 'show'))
+        ->toEqual('user-show');
+});
+
+test('name: fail', function () {
+    expect(fn() => UserPermission::name(UserPermission::USER, 'populate'))
+        ->toThrow('Action: populate does not exists in: show,store,update,delete,restore');
+});
+
+test('list: exclude references & actions', function () {
+    expect(UserPermission::list(
+        excludeReferences: ['company'],
+        excludeActions: ['show'],
+    ))->toMatchArray(
+        Collection::make(UserPermission::cases())
+            ->reject(fn($case) => $case->value === 'company')
+            ->map(function ($reference) {
+                return Collection::make(UserPermission::actions())
+                    ->except('show')
+                    ->map(fn($action) => $reference->value . '-' . $action)
+                    ->values();
+            })->flatten()->toArray()
+    );
+});
+
+test('list: only references & actions', function () {
+    expect(UserPermission::list(
+        onlyReferences: ['company'],
+        onlyActions: ['show'],
+    ))->toMatchArray([
+        'company-show'
+    ]);
+});
+
+# this test represent a sample of all 3 different types of data structure
 test('tableStructure: showcases 3 different data structure used to assemble data for table', function () {
     $user = User::factory()->create();
     $user->assignRole(UserRole::USER);
